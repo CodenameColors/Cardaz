f83963c4c7835fe0fe8a5ac42efc72088c8a1b3f
psychoticpolice <antmorale@hartford.edu>
2018-03-31 15:26:24 -0400

    Added routes for all of the database

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -10,11 +10,58 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//here is all the routes to the API
+
+/*
+*	ADDRESS ROUTES
+*/
+router.get('/api/address', db.getAllAddresses);
+router.get('/api/address/:id', db.getOneAddress);
+router.post('/api/address', db.createAddress);
+router.put('/api/address/:id', db.updateAddress)
+router.delete('/api/address/:id', db.removeAddress)
+
+
+/*
+*	PATIENT ROUTES
+*/
+router.get('/api/patient', db.getAllPatients);
+router.get('/api/patient/:id', db.getOnePatient);
+router.post('/api/patient', db.createPatient);
+router.put('/api/patient/:id', db.updatePatient)
+router.delete('/api/patient/:id', db.removePatient)
+
+
+/*
+*	HeartData ROUTES
+*/
+router.get('/api/heartrecord', db.getAllHeartrecords);
+router.get('/api/heartrecord/:id', db.getOneHeartrecord);
+router.post('/api/heartrecord', db.createHeartrecord);
+router.put('/api/heartrecord/:id', db.updateHeartrecord)
+router.delete('/api/heartrecord/:id', db.removeHeartrecord)
+
+
+/*
+*	The records that will be displayed on the website. routing
+*/
+router.get('/api/record', db.getAllRecords);
+router.get('/api/record/:id', db.getOneRecord);
+router.post('/api/record', db.createRecord);
+router.put('/api/record/:id', db.updateRecord)
+router.delete('/api/record/:id', db.removeRecord)
+
+
+
+// OLD TESTING DELETE THIS LATER.
+/*
 router.get('/api/puppies', db.getAllPuppies);
 router.get('/api/puppies/:id', db.getSinglePuppy);
 router.post('/api/puppies/', db.createPuppy);
 router.put('/api/puppies/:id', db.updatePuppy);
 router.delete('/api/puppies/:id', db.removePuppy);
+*/
 
 module.exports = router;
 
