c3acf19367d7e78c468e50bbb06a45906778baaa
psychoticpolice <antmorale@hartford.edu>
2018-04-15 18:37:47 +0000

    started pi app dev

--- piCode/cardaz_app/main.py
+++ piCode/cardaz_app/main.py
@@ -2,6 +2,10 @@
 #RUN THIS COMMAND ANYTIME THE FORM IN QT CHANGES
 #   pyuic5 cardaz_app.ui > cardaz_app.py
 
+#HEY FUTURE TIRED ME!! if the error for can't find pyqtgraph appears
+#chang eth cardaz_app.py file to this
+#      
+#          from pyqtgraph.pyqtgraph import PlotWidget
 
 #needs this always
 import sys
@@ -10,24 +14,55 @@ import sys
 import PyQt5
 from PyQt5.QtWidgets import *
 
+from random import *
+
 #get form code.
 import cardaz_app
+
+import pyqtgraph as pg
 
 #create class for the GUI
 class Form1(QMainWindow, cardaz_app.Ui_cardaz_app):
     
     #define button callback
     def pressedStartButton(self):
-        print(self.first_name_TB.text())
+        print("first name:  " + self.first_name_TB.text() + "\n")
+        print("last name:  " + self.last_name_TB.text() + "\n")
+        self.form_tabcontrol.setCurrentIndex(3)
         #print("testing")
     
+    #define button callback
+    def pressedYes(self):
+        self.form_tabcontrol.setCurrentIndex(2)
+    
+    #define button callback
+    def pressedNo(self):
+        self.form_tabcontrol.setCurrentIndex(1)
+    
+    #define button callback
+    def pressedBack(self):
+        self.form_tabcontrol.setCurrentIndex(0)
+        
+    #define button callback
+    def pressedReady(self):
+        self.form_tabcontrol.setCurrentIndex(3)
+        pitch = [.0,.200,.300,.400,.500,.600]
+        self.heart_graph.plot(pitch)
+        
     
     def __init__(self):
         super(self.__class__,self).__init__()
         self.setupUi(self) #defined in the UI file
+        self.form_tabcontrol.setCurrentIndex(0)#sets first page
         
         #setup callback functions
         self.execute_BTN.clicked.connect(lambda: self.pressedStartButton())
+        self.yesacc_BTN.clicked.connect(lambda: self.pressedYes())
+        self.noacc_BTN.clicked.connect(lambda: self.pressedNo())
+        self.back_BTN.clicked.connect(lambda: self.pressedBack())
+        self.ready_BTN.clicked.connect(lambda: self.pressedReady())
+        
+        
         
     #main function
 def main():
