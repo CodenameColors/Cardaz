34960a77d052952790eae431e2b3f26ced0b2db9
psychoticpolice <antmorale@hartford.edu>
2018-04-16 03:17:23 +0000

    added psql support

--- piCode/cardaz_app/main.py
+++ piCode/cardaz_app/main.py
@@ -21,6 +21,8 @@ import cardaz_app
 
 import pyqtgraph as pg
 
+import psycopg2
+
 #create class for the GUI
 class Form1(QMainWindow, cardaz_app.Ui_cardaz_app):
     
@@ -43,6 +45,92 @@ class Form1(QMainWindow, cardaz_app.Ui_cardaz_app):
     def pressedBack(self):
         self.form_tabcontrol.setCurrentIndex(0)
         
+    def gotoTestForm(self):
+        self.form_tabcontrol.setCurrentIndex(4)
+        
+    #PSQL -------------------------------------------------'
+    
+    #this method is used to gather all the records.
+    def selectAllRecords(self):
+        conn = psycopg2.connect(host='localhost', database='cardaz', user='antonio', password='19528974')
+        print("connecting to PSQL database")
+        cur = conn.cursor()
+        
+        query = 'SELECT patient.id, patient.first_name, patient.mi, patient.last_name, patient.phone_number'
+        query +=', address.street, address.city, address.state, address.zip_code'
+        query +=' FROM patient inner join address ON patient.id = address.patient_id;'
+        
+        test = cur.execute(query)
+        for record in cur:
+            print(record)
+        
+        
+    def selectFilter(self):
+        conn = psycopg2.connect(host='localhost', database='cardaz', user='antonio', password='19528974')
+        print("connecting to PSQL database")
+        cur = conn.cursor()
+        
+        query = 'SELECT patient.id, patient.first_name, patient.mi, patient.last_name, patient.phone_number'
+        query +=', address.street, address.city, address.state, address.zip_code'
+        query +=' FROM patient inner join address ON patient.id = address.patient_id'
+        query += " where LOWER(patient.first_name) Like '%s';" % ("ant%")
+        
+        test = cur.execute(query)
+        for record in cur:
+            print(record)
+                
+    def insertRecord(self):
+        conn = psycopg2.connect(host='localhost', database='cardaz', user='antonio', password='19528974')
+        print("connecting to PSQL database")
+        cur = conn.cursor()
+        
+        query = 'INSERT into patient(first_name, mi, last_name, phone_number)'
+        query +=" Values('%s', '%s', '%s', '%s') returning id;" %  ('luis', 'A', 'morales', '5089880582')
+        
+        test = cur.execute(query)
+        conn.commit()
+        id = 0;
+        for record in cur:
+            print(record)
+            id = record[0]
+        
+        query = 'INSERT into address(patient_id, street, city, state, zip_code)'
+        query +=" Values(%d, '%s', '%s', '%s', '%s') returning id;" %  (id,'522 Propect St', 'Methuen', 'Ma', '01844')
+        
+        test = cur.execute(query)
+        conn.commit()
+        id = 0;
+        for record in cur:
+            print(record)
+            id = record[0]
+        
+    def updateRecord(self):
+        conn = psycopg2.connect(host='localhost', database='cardaz', user='antonio', password='19528974')
+        print("connecting to PSQL database")
+        cur = conn.cursor()
+        
+        query = "update patient set first_name='%s', mi='%s' , last_name='%s', phone_number='%s' where id =%d;" % ('garbo', 'D', 'Trash', 'DNE', 19)
+        test = cur.execute(query)
+        conn.commit()
+        
+        query = "update address set street='%s', city='%s' , state='%s', zip_code='%s' where patient_id =%d;" % ('garbo', 'D', 'Trash', 'DNE', 19)
+        test = cur.execute(query)
+        conn.commit()
+    
+    def removeRecord(self):
+        conn = psycopg2.connect(host='localhost', database='cardaz', user='antonio', password='19528974')
+        print("connecting to PSQL database")
+        cur = conn.cursor()
+    
+        query = "delete from address where patient_id = %d" % (19)
+        test = cur.execute(query)
+        conn.commit()
+    
+        query = "delete from patient where id = %d" % (19)
+        test = cur.execute(query)
+        conn.commit()
+        
+    
     #define button callback
     def pressedReady(self):
         self.form_tabcontrol.setCurrentIndex(3)
@@ -61,8 +149,13 @@ class Form1(QMainWindow, cardaz_app.Ui_cardaz_app):
         self.noacc_BTN.clicked.connect(lambda: self.pressedNo())
         self.back_BTN.clicked.connect(lambda: self.pressedBack())
         self.ready_BTN.clicked.connect(lambda: self.pressedReady())
-        
-        
+        self.testformBTN.clicked.connect(lambda: self.gotoTestForm())
+        self.exitBTN.clicked.connect(lambda: self.pressedBack())
+        self.sBTN.clicked.connect(lambda: self.selectAllRecords())
+        self.sfilterBTN.clicked.connect(lambda: self.selectFilter())
+        self.insBTN.clicked.connect(lambda: self.insertRecord())
+        self.upBTN.clicked.connect(lambda: self.updateRecord())
+        self.remBTN.clicked.connect(lambda: self.removeRecord())
         
     #main function
 def main():
